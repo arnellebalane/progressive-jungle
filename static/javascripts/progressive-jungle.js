99ad305daf4efb189e61d763d742022350d722ba
Arnelle Balane <arnellebalane@gmail.com>
2016-07-28 02:28:13 +0800

    add message screen and transition between it and login screen

--- static/javascripts/progressive-jungle.js
+++ static/javascripts/progressive-jungle.js
@@ -22,16 +22,35 @@ var toast = (function() {
 
 
 
-$('.button--google').on('click', function() {
+$('.login-button').on('click', function() {
     $(this).addClass('button--loading');
     toast.close();
 
     setTimeout(function() {
         if (Math.random() < 0.5) {
-            toast.open('Successfully logged in!');
-            $('.screen--login').addClass('screen--maximized');
+            toast.open('You are now logged in.');
+            $('.login-screen').addClass('screen--maximized');
+            $('.message-screen').removeClass('screen--minimized');
+            $(this).removeClass('button--loading');
         } else {
             toast.open('Login failed. Please try again.');
+            $(this).removeClass('button--loading');
+        }
+    }.bind(this), 1000);
+});
+
+
+$('.logout-button').on('click', function() {
+    $(this).addClass('button--loading');
+
+    setTimeout(function() {
+        if (Math.random() < 0.5) {
+            toast.open('You are now logged out.');
+            $('.login-screen').removeClass('screen--maximized');
+            $('.message-screen').addClass('screen--minimized');
+            $(this).removeClass('button--loading');
+        } else {
+            toast.open('Logout failed. Please try again.');
             $(this).removeClass('button--loading');
         }
     }.bind(this), 1000);
